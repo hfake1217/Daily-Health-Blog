2b2ab27a50ebae260db87af6eacd5aa38c5ab2ea
Hayley Fake <hayleyfake@comcast.net>
2023-07-05 13:05:01 -0400

    edit content for steps page

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const _ = require("lodash");
 const mongoose = require('mongoose');
 
 const homeStartingContent = "Below is where you keep track of your daily workouts. Please record the date of your workout as the title, and record the exercises, reps, sets, and weights used during your workout. Click below to add a workout.";
-const aboutContent = "This is where you will keep track of your daily meals and snacks. Please record the date of your entry as the title. Then use the text box to list out food items with the total calorie content for the day and the total grams of protein, carbohydrates, and fats consumed for the day.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const nutritionContent = "This is where you will keep track of your daily meals and snacks. Please record the date of your entry as the title. Then use the text box to list out food items with the total calorie content for the day and the total grams of protein, carbohydrates, and fats consumed for the day.";
+const stepsContent = "This section is where you will log your daily step count at the end of the day. This is to help keep track of daily movement.";
 
 const app = express();
 
@@ -35,12 +35,12 @@ app.get("/", function(req, res){
   });
 });
 
-app.get("/about", function(req, res){
-  res.render("about", {aboutContent: aboutContent});
+app.get("/nutrition", function(req, res){
+  res.render("nutrition", {nutritionContent: nutritionContent});
 });
 
-app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactContent});
+app.get("/steps", function(req, res){
+  res.render("steps", {stepsContent: stepsContent});
 });
 
 app.get("/compose", function(req, res){
